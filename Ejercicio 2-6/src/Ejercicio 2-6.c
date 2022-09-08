b1a170278901604377bdf9141761c26a0d28877a
unknown <tomas26013@gmail.com>
2022-09-08 10:29:19 -0300

    2-6

---
+++ Ejercicio 2-6/src/Ejercicio 2-6.c
@@ -0,0 +1,41 @@
+/*
+ ============================================================================
+ Name        : Ejercicio.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+//Tomas Mastrapasqua
+int main(void) {
+
+	setbuf(stdout,NULL);
+
+	char letra;
+	int i;
+	int mayusculas=0;
+	int numeros;
+
+
+	for(i=0;i<5;i++)
+	{
+		printf("Ingresar un caracter: ");//hay error
+		scanf("%c", &letra);
+
+
+		if(letra == 'm')
+		{
+			mayusculas++;
+		}
+	}
+
+	printf("Se ingresaron %d letras 'm' minusculas ", mayusculas);
+
+
+
+	return EXIT_SUCCESS;
+}
